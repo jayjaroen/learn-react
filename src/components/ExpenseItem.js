f2583e86906c80bd4d473349bf57eb3c120a022c
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2023-01-15 21:33:52 +0700

    learn jsx and add css stying

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem(){
-  return <h2>Expense Item</h2>
+import './ExpenseItem.css';
+
+function ExpenseItem() {
+  const expenseDate = new Date(2022, 1, 15); // date object
+  const expenseTitle = 'Food';
+  const expenseAmount = 2200.55;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString()}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">{expenseAmount}</div>
+      </div>
+    </div>)
 }
 
 export default ExpenseItem;
