0af38994d96d570749d81455e4cf5ad8df871b3f
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2023-01-16 22:28:57 +0700

    learn props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,15 +1,19 @@
 import './ExpenseItem.css';
 
-function ExpenseItem() {
-  const expenseDate = new Date(2022, 1, 15); // date object
-  const expenseTitle = 'Food';
-  const expenseAmount = 2200.55;
+function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-us', { month: "long" });
+  const day = props.date.toLocaleString('en-us', { day: "2-digit" });
+  const year = props.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{expenseDate.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{day}</div>
+        <div>{year}</div>
+      </div>
       <div className="expense-item__description">
-        <h2>{expenseTitle}</h2>
-        <div className="expense-item__price">{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">{props.amount}</div>
       </div>
     </div>)
 }
