76684543ef9f6c929971cd5dabf8676cef59377b
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2023-01-17 22:21:05 +0700

    spliting component into multiple components

--- src/App.js
+++ src/App.js
@@ -25,8 +25,10 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
+      {/* can be self-closing tag, if there's no content between openning and closing tags, below it works both way */}
+      {/* ExpenseItem component using the date data in the app component */}
+      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
+      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
       <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
     </div>
