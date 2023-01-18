46baeff9a8fe02e0d62b455696c7ba7292bb60c9
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2023-01-18 22:16:03 +0700

    finished practice 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -27,10 +27,7 @@ function App() {
       <h2>Let's get started!</h2>
       {/* can be self-closing tag, if there's no content between openning and closing tags, below it works both way */}
       {/* ExpenseItem component using the date data in the app component */}
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-      <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem>
+      <Expenses expenses={expenses} />
     </div>
   );
 }
