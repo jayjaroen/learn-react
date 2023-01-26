31f655899e3c7900bdf08c6cde07577011ac37d2
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2023-01-26 21:57:29 +0700

    learn useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,16 @@
+import React, { useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card'; //the concept of composition(Children props)
 
 function ExpenseItem(props) {
+  // useState return an array: 1) the value of the variable itself; 2) second element is updating function
+  // JS data destructure
+  // useState call inside of the component function, not nested function
+  const [title, setTitle] = useState(props.title);
   const clickHandler = () => {
-    console.log('hahahahha');
+    setTitle('Updated'); // pass an argument
+    console.log(title); // don't change the value right away but schedule the state update
   }
   return (
     <Card className="expense-item">
@@ -12,7 +18,7 @@ function ExpenseItem(props) {
       {/* passing data through different components via props */}
       {/* here passing data from App to ExpenseDate via ExpenseItem */}
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
       <button onClick={clickHandler}>Click Here</button>
